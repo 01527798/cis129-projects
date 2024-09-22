b50612f299ec0cc839ae9b1d7f08694b283ef27a
01527798 <kchung5@mail.pima.edu>
2024-09-22 15:54:41 -0700

    Update cis129_lab02_shortStory.py
    I have changed the output of "Hello world!" to a short story about the struggles of having a faulty laptop battery.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+#Author: Kaitlyn Chung
+#My first Python lab in my CIS129 course!
+#Prints a simple story about what has happened in the last three hours.
+print("My laptop battery drains quickly, making it difficult to use it to do anything where I am not tethered to a wall.")
+print("So, when I decide to do work outside of home the only requirement I have is that there is easy outlet access.")
+print("I am current back in Tucson, working on these assignments in a coffee shop in Oro Valley.")
+print("And I have changed my seat three times to get closer to an outlet.")
